25d0e32eae19e24d1a61fc106fb8c4eb9c181152
Aubacca <aubacca.renhor@gmail.com>
2018-02-19 17:35:41 +0100

    Initial version.

---
+++ src/app/weather-page/weather-page.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-weather-page",
+  template: `
+      <div class="container">
+        <form (ngSubmit)="doSubmit(citySearch)" #citySearch="ngForm">
+          <label for="">Stadt:</label>
+          <input type="text" name="cityName" ngModel placeholder="Enter name of the city to be searched for..">
+          <br>
+          <input type="submit" value="Search">
+        </form>
+      </div>
+    `,
+  styleUrls: ["./weather-page.component.css"]
+})
+export class WeatherPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+
+  doSubmit(searchValues) {
+    console.log("Form submitted! searchValue: ", searchValues);
+    console.log("Form submitted! cityName: ", searchValues.value.cityName);
+  }
+}
